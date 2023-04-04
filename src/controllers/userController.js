9dd6242e3a65f971e385ba45bac466273795eb79
Chetan-Singh-Chouhan <mr.chiragchouhan@gmail.com>
2023-04-04 15:02:32 +0530

    first push

---
+++ src/controllers/userController.js
@@ -0,0 +1,65 @@
+const userModel = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+//========================================Create User==================================================================//
+const createUser = async function (req, res) {
+try {
+    let body = req.body;
+    if (Object.keys(body).length == 0) return res.status(400).send({ status: false, message: 'please enter all required details to register a user' });
+    let { fname, lname, email, phone, password } = req.body;
+    if (!fname) return res.status(400).send({ status: false, message: 'fname is required, please enter fname to register a user' });
+    if (!lname) return res.status(400).send({ status: false, message: 'lname is required, please enter lname to register a user' });
+    if (!email) return res.status(400).send({ status: false, message: 'email is required, please enter email to register a user' });
+    if (!phone) return res.status(400).send({ status: false, message: 'phone number is required, please enter phone number to register a user' });
+    if (!password) return res.status(400).send({ status: false, message: 'password is required, please enter password to register a user' });
+
+    // password hashing ----------------
+    const saltRound = 5;
+    bcrypt.hash(password, saltRound, function (err, hash) {
+        body.password = hash;
+    });
+
+    // for unique email and phone -------------
+    let user = {};
+    user = await userModel.findOne({ email: email })
+    if (user) return res.status(409).send({ status: false, message: 'email already in use, please enter a unique email to register a user' });
+    user = await userModel.findOne({ phone: phone });
+    if (user) return res.status(409).send({ status: false, message: 'phone number is already in use, please enter a unique phone number to register a user' });
+   
+
+    const userCreated = await userModel.create(body);
+    res.status(201).send({ status: true, message: 'User created successfully', data: userCreated });
+   
+    } catch (error) {
+        res.status(500).send({ status: false, message: error.message });
+    }
+};
+//===================================================Login User========================================================//
+const userLogin = async function (req, res) {
+    try {
+        const body = req.body;
+        const { email, password } = body;
+        if (!email) return res.status(400).send({ status: false, message: 'email is required, please enter email to login a user' });
+        if (!password) return res.status(400).send({ status: false, message: 'password is required, please enter password to login a user' });
+        const user = await userModel.findOne({ email: email });
+        if (!user) return res.status(400).send({ status: false, message: 'email is incorrect' });
+        
+        bcrypt.compare(password, user.password, function (err, result) {
+            if (result) {
+                let payload = { userId: user._id, iat: Date.now() }
+                let token = jwt.sign(payload, "group16")
+                let decodedToken = jwt.verify(token, "group16");
+                let userId = decodedToken.userId
+                res.status(200).send({ status: true, message: 'user logged in successfully', data: { userId: userId, token: token } })
+            }
+            else { return res.status(400).send({ status: false, message: 'password is incorrect' }) }
+        })
+
+    }
+    catch (error) {
+        return res.status(500).send({ status: false, message: error.message })
+    }
+};
+
+module.exports = { createUser, userLogin};
---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const route = require('./routes/route.js');
+const bodyParser = require("body-parser");
+const app = express();
+const mongoose = require('mongoose');
+
+
+app.use(bodyParser.json());
+
+mongoose.set("strictQuery", false);
+mongoose.connect("mongodb+srv://Chetan_ProjectClustor:PNr1Fn8OcRu2cGmk@project1.h4p8xqh.mongodb.net/FSOC_Project_DB", { useNewUrlParser: true })
+    .then(() => console.log("Mongo db is connected"))
+    .catch(err => console.log(err))
+app.use('/', route);
+
+app.listen(3000, function () {
+    console.log('Express app running on port ' + 3000)
+});
