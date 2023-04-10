82cfda9612508ce9367327e08e80437727d83205
Chetan-Singh-Chouhan <mr.chiragchouhan@gmail.com>
2023-04-10 22:51:03 +0530

    phase 2

---
+++ src/models/blogModel.js
@@ -0,0 +1,30 @@
+const mongoose =require('mongoose')
+const ObjectID= mongoose.Schema.Types.ObjectID
+const blogsSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required: true
+    },
+    body:{
+        type:String,
+        required: true
+    },
+    userId:{
+        type: ObjectID,
+        ref: "user",
+        required: true
+    },
+    category:{
+        type:String,
+        required: true
+    },
+    deletedAt:
+    {
+        type:Date
+    },
+    isDeleted:{
+        type:Boolean,
+        default:false
+    },  
+},{timestamps:true})
+module.exports = mongoose.model("blog",blogsSchema) 
--- src/routes/route.js
+++ src/routes/route.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const { createUser, userLogin } = require('../controllers/userController');
+const {createblog,getblog,updateBlogData} = require("../controllers/blogController")
 
-router.post("/register", createUser);
-router.post("/login", userLogin);
+router.post("/register", createUser); // createUser
+router.post("/login", userLogin); //register user
+router.post("/blogs",createblog) //create Blog
+router.get("/blogs",getblog) //get Blog
+router.put("/blogs/:blogId",updateBlogData)  //update the blog
 
 module.exports = router;
